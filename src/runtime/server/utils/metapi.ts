0bf1f2ecee79ddf4b264102a1e34f0fda8645cef
kevin olson <acidjazz@gmail.com>
2025-01-07 00:23:16 -0600

    ♻️ changing things up big time

--- src/runtime/server/utils/metapi.ts
+++ src/runtime/server/utils/metapi.ts
@@ -1,14 +1,15 @@
-import { setResponseStatus } from 'h3'
 import type { H3Event } from 'h3'
+import { setResponseStatus } from 'h3'
+import type { MetapiDetail, MetapiResponse } from '#api-utils'
 
 let start: number | undefined
 
-const bench = (): string => {
+function bench(): string {
   const end = performance.now()
   return start ? `${(end - start).toFixed(3)}ms` : 'n/a'
 }
 
-const render = (data: unknown): import('#api-utils').MetapiResponse => {
+function render(data: unknown): MetapiResponse {
   return {
     meta: {
       benchmark: bench(),
@@ -18,7 +19,7 @@ const render = (data: unknown): import('#api-utils').MetapiResponse => {
   }
 }
 
-const success = (message: string, data?: unknown): import('#api-utils').MetapiResponse => {
+function success(message: string, data?: unknown): MetapiResponse {
   return {
     meta: {
       benchmark: bench(),
@@ -29,7 +30,7 @@ const success = (message: string, data?: unknown): import('#api-utils').MetapiRe
   }
 }
 
-const error = (event: H3Event, detail: import('#api-utils').MetapiDetail, code: number = 400): import('#api-utils').MetapiResponse => {
+function error(event: H3Event, detail: MetapiDetail, code: number = 400): MetapiResponse {
   setResponseStatus(event, code)
   return {
     meta: {
@@ -41,15 +42,22 @@ const error = (event: H3Event, detail: import('#api-utils').MetapiDetail, code: 
   }
 }
 
-const notFound = (event: H3Event): import('#api-utils').MetapiResponse => error(event, 'Not Found', 404)
+function notFound(event: H3Event): MetapiResponse {
+  return error(event, 'Not Found', 404)
+}
 
-const renderNullError = (event: H3Event, data: unknown): import('#api-utils').MetapiResponse => {
+function renderNullError(event: H3Event, data: unknown): MetapiResponse {
   if (data === null) return error(event, 'Not Found', 404)
   return render(data)
 }
 
-const metapi = () => {
+export default function metapi() {
   start = performance.now()
-  return { render, success, error, renderNullError, notFound }
+  return {
+    render,
+    success,
+    error,
+    renderNullError,
+    notFound,
+  }
 }
-export default metapi
