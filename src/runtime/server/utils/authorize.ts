1d9cfe0cdea9880b40592ecd64c6ba3d669ebc93
kevin olson <acidjazz@gmail.com>
2025-01-17 22:07:19 -0600

    🐛 fix issue with async policies

--- src/runtime/server/utils/authorize.ts
+++ src/runtime/server/utils/authorize.ts
@@ -1,9 +1,10 @@
-type PolicyFunction<T extends object> = (args: T) => boolean
+type PolicyFunction<T extends object> = (args: T) => boolean | Promise<boolean>
 
 export async function authorize<T extends object>(
   policyFunction: PolicyFunction<T>,
   args: T,
 ): Promise<void> {
-  if (!policyFunction(args))
+  if (!await Promise.resolve(policyFunction(args))) {
     throw createError({ statusCode: 401, statusMessage: 'Unauthorized' })
+  }
 }
